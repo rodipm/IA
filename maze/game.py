ef54c703fc19e8f0fedaf0acb8c4f2d4d87598d9
rodipm <snnlion@gmail.com>
2019-09-08 20:49:39 -0300

    Implementado Q-Learning no Maze; Adicionado MountaiCar-v0

--- maze/game.py
+++ maze/game.py
@@ -9,7 +9,7 @@ menu_height = 100
 size = width, height  + menu_height
 
 screen = pygame.display.set_mode(size)
-pygame.display.set_caption("Maze - AI PLayground")
+pygame.display.set_caption("Maze - AI PLay")
 
 black = (0, 0, 0)
 white = (100, 100, 100)
@@ -217,46 +217,6 @@ class Queue:
     def isEmpty(self):
         "Returns true if the queue is empty"
         return len(self.list) == 0
-
-
-class PriorityQueue:
-    """
-      Implements a priority queue data structure. Each inserted item
-      has a priority associated with it and the client is usually interested
-      in quick retrieval of the lowest-priority item in the queue. This
-      data structure allows O(1) access to the lowest-priority item.
-    """
-
-    def __init__(self):
-        self.heap = []
-        self.count = 0
-
-    def push(self, item, priority):
-        entry = (priority, self.count, item)
-        heapq.heappush(self.heap, entry)
-        self.count += 1
-
-    def pop(self):
-        (_, _, item) = heapq.heappop(self.heap)
-        return item
-
-    def isEmpty(self):
-        return len(self.heap) == 0
-
-    def update(self, item, priority):
-        # If item already in priority queue with higher priority, update its priority and rebuild the heap.
-        # If item already in priority queue with equal or lower priority, do nothing.
-        # If item not in priority queue, do the same thing as self.push.
-        for index, (p, c, i) in enumerate(self.heap):
-            if i == item:
-                if p <= priority:
-                    break
-                del self.heap[index]
-                self.heap.append((priority, c, item))
-                heapq.heapify(self.heap)
-                break
-        else:
-            self.push(item, priority)
 
 
 def depth_first_search(startState):
